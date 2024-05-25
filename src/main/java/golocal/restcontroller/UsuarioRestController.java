72ca81857953b66458002eb51a3d0c7cc1ca10a8
Carol <carolbrazalessempere@gmail.com>
2024-05-25 13:49:33 +0200

    Add service user registration

--- src/main/java/golocal/restcontroller/UsuarioRestController.java
+++ src/main/java/golocal/restcontroller/UsuarioRestController.java
@@ -1,43 +1,38 @@
 package golocal.restcontroller;
 
-import java.util.List;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import golocal.modelo.entity.Rol;
+import golocal.modelo.dto.UserSignUpDTO;
 import golocal.modelo.entity.Usuario;
-import golocal.service.UsuarioService;
+import golocal.repository.UsuarioRepository;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
-@RequestMapping("/usuario")
 public class UsuarioRestController {
 
 	@Autowired
-	private UsuarioService usuarioService;
-	
-	
-	/*
-	@GetMapping("/porBusqueda/{nombreCiudad}/{nombrePais}/{fechaDisponible}")
-    public List<Usuario> obtenerItinerariosPorCiudadPaisYFecha(
-            @PathVariable("nombreCiudad") String nombreCiudad,
-            @PathVariable("nombrePais") String nombrePais,
-            @PathVariable("fechaDisponible")@DateTimeFormat(pattern="yyyy-MM-dd") Date fechaDisponible) {
-        return usuarioService.findGuiasByCiudadPaisFecha(nombreCiudad, nombrePais, fechaDisponible);
-    }
-    */
-	
-	
-	
-	
-	
+	private UsuarioRepository usuarioRepository;
+
+	@PostMapping("/signup")
+	public ResponseEntity<String> registerUser(@RequestBody UserSignUpDTO userRegistrationDTO) {
+
+		Usuario usuario = new Usuario();
+		usuario.setNombre(userRegistrationDTO.getNombre());
+		usuario.setApellidos(userRegistrationDTO.getApellidos());
+		usuario.setEmail(userRegistrationDTO.getEmail());
+		usuario.setTelefono(userRegistrationDTO.getTelefono());
+		usuario.setPassword(userRegistrationDTO.getContrasena());
+		usuario.setDni(userRegistrationDTO.getDni());
+		usuario.setUsername(userRegistrationDTO.getUsername());
+		usuario.setSobreMi(userRegistrationDTO.getSobreMi());
+
+		usuarioRepository.save(usuario);
+
+		return ResponseEntity.ok("Usuario registrado con éxito");
+	}
+
 }
