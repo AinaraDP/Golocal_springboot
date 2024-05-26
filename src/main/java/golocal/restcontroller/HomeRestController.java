70aa232cb52c7ec0727a2d40cd579e455a53d2cd
Carol <carolbrazalessempere@gmail.com>
2024-05-26 21:13:53 +0200

    minor changes

--- src/main/java/golocal/restcontroller/HomeRestController.java
+++ src/main/java/golocal/restcontroller/HomeRestController.java
@@ -2,7 +2,11 @@ package golocal.restcontroller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,19 +21,7 @@ public class HomeRestController {
 	@Autowired
 	private UsuarioService usuarioService;
 
-	/*
-	@PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody UsuarioDto usuarioDto) {
-        String usuario = usuarioDto.getUsername();
-        String password = usuarioDto.getPassword();
-
-        if (usuarioService.verificarCredenciales(usuario, password)) {
-            return ResponseEntity.ok().body("Login exitoso");
-        } else {
-            return ResponseEntity.status(401).body("Credenciales incorrectas");
-        }
-    }
-	*/
+	
 	
 	@PostMapping("/login")
 	public ResponseEntity<String> login(@RequestBody UsuarioDto usuarioDto) {
@@ -44,7 +36,9 @@ public class HomeRestController {
 	    if (!usuarioService.verificarCredenciales(usuario, password)) {
 	        return ResponseEntity.status(401).body("Contraseña incorrecta");
 	    }
-
+	    
+	   
 	    return ResponseEntity.ok().body("Login exitoso");
 	}
+	
 }
